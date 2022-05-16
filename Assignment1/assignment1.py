e271928da825c1323b17bd966e59a88b00ddfe34
IJsbarnd <i.j.a.pool@st.hanze.nl>
2022-05-16 11:41:27 +0200

    finished assignment1

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -1,64 +1,104 @@
-#!/usr/bin/env python3
+#! usr/bin/env python3
 
+"""
+assignment1.py
+First assignment of Big Data Computing
+Calculates the average PHRED score per base position for all reads within a fastq file.
+"""
+
+_author_ = "IJsbrand Pool"
+_version_ = 2.0
 
 import argparse as ap
 import multiprocessing as mp
+import csv
 import sys
 
 
+def chunks(number, mysize):
+    """ Returns the chunks. """
+    mychunks = []
+    for i in range(mysize):
+        start = int(i * len(number) / mysize)
+        end = int((i + 1) * len(number) / mysize)
+        mychunks.append(number[start:end])
 
-argparser = ap.ArgumentParser(description="Script voor Opdracht 1 van Big Data Computing")
-argparser.add_argument("-n", action="store",
-                       dest="n", required=True, type=int,
-                       help="Aantal cores om te gebruiken.")
-argparser.add_argument("--output","-o",
-                       required=False, help="CSV file om de output in op te slaan. Default is output naar terminal STDOUT")
-argparser.add_argument("fastq_files", action="store", nargs='+', help="Minstens 1 Illumina Fastq Format file om te verwerken")
-args = argparser.parse_args()
+    return mychunks
 
 
+def read_fastq(fastq_file):
+    """ Reads the files"""
+    quality = []
+    qual = True
 
-def doeIets(fastqfile, csvfile, start=0, chunk=0):
-    with open(fastqfile, 'r', encoding='utf8') as fastq:
-        if chunk == 0:
-            chunk = len(fastq.readlines()) -1
-        fastq.seek(0)
-        #fastforward
-        #start = 0
-        i = 0
-        while i < start:
-            fastq.readline()
-            i += 1
-
-        results = []
-        counter = 0
-        while counter < chunk:
+    with open(fastq_file, encoding='UTF-8') as fastq:
+        while qual:
             header = fastq.readline()
             nucleotides = fastq.readline()
             strand = fastq.readline()
-            qual = fastq.readline()
-            counter += 4
+            qual = fastq.readline().rstrip()
+            if qual:
+                quality.append(qual)
+            if header or nucleotides or strand:
+                pass
 
-            if not(qual):
-                # we reached the end of the file
-                break
-            for j, c in enumerate(qual):
+    return quality
 
-                try:
-                    results[j] += ord(c) - 33
-                except IndexError:
-                    results.append(ord(c) - 33)
 
-    iets = [(phredscore / (counter / 4)) for phredscore in results]
-    print(iets)
-    with open(csvfile, 'w') as csv:
-        for number in range(len(iets) - 1):
-            csv.write(str(number) + ',' + str(iets[number]) + "\n")
+def calculate_quals(quality):
+    """ Calculates quality scores """
+    results = []
+    for qual in quality:
+        for item, checker in enumerate(qual):
+            try:
+                results[item] += ord(checker) - 33
+            except IndexError:
+                results.append(ord(checker) - 33)
+    return results
+
+
+def generate_output(average_phredscores, csvfile):
+    """ Generates the output for the file('s) """
+    if csvfile is None:
+        csv_writer = csv.writer(sys.stdout, delimiter=',')
+        for i, score in enumerate(average_phredscores):
+            csv_writer.writerow([i, score])
+
+    else:
+        with open(csvfile, 'w', encoding='UTF-8', newline='') as myfastq:
+            csv_writer = csv.writer(myfastq, delimiter=',')
+            for i, score in enumerate(average_phredscores):
+                csv_writer.writerow([i, score])
 
 
 if __name__ == '__main__':
-    fastqfiles = args.fastq_files
-    csv = args.output
-    for file in fastqfiles:
-        doeIets(file, csv)
-    sys.exit(0)
+    # argparse
+    argparser = ap.ArgumentParser(description="Script for assignment 1 of Big Data Computing")
+    argparser.add_argument("-n", action="store",
+                           dest="n", required=True, type=int,
+                           help="Amount of cores to be used")
+    argparser.add_argument("-o", action="store", dest="CSVfile",
+                           required=False, help="CSV file to save the output.")
+    argparser.add_argument("fastq_files", action="store",
+                           nargs='+', help="At least 1 ILLUMINA fastq file to process")
+    args = argparser.parse_args()
+
+    for file in args.fastq_files:
+        qualities = read_fastq(file)
+        qual_chunked = chunks(qualities, 4)
+
+        with mp.Pool(args.n) as pool:
+            phredscores = pool.map(calculate_quals, qual_chunked)
+
+        phredscores_avg = [sum(i) / len(qualities) for i in zip(*phredscores)]
+
+        if len(args.fastq_files) > 1:
+            if args.CSVfile is None:
+                sys.stdout.write(file + "\n")
+                CSV = None
+            else:
+                CSV = f'{file}.{args.CSVfile}'
+        else:
+            CSV = args.CSVfile
+
+        generate_output(phredscores_avg, CSV)
